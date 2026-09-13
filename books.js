4080b953d3b40ef566afa72753ac80edd0b8bd81
bunpuku-hitone <hitone.shokufuku@gmail.com>
2026-09-14 08:55:28 +0900

    ダイジェスト版15冊と次ページ用のデータ構造を作成

---
+++ books.js
@@ -0,0 +1,10 @@
+const BOOKS = [
+  // ダイジェスト版が出版されたら、ここへ新刊順に追加します。
+  // 最大15冊を表示します。
+  // 例:
+  // {"no":1,"status":"published","title":"書名","image":"book1.jpg","amazon":"https://www.amazon.co.jp/dp/ASIN"}
+];
+
+// 2ページ目を作成した後、そのWebカタログURLを設定します。
+// 例: "https://bunpuku-hitone.github.io/hitone-books-digest-2/"
+const NEXT_CATALOG_URL = "";
