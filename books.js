8366ec6acce93c3f2facb312b02654ce4f82d854
bunpuku-hitone <hitone.shokufuku@gmail.com>
2026-09-16 22:37:41 +0900

    99円シリーズ6冊目を登録

--- books.js
+++ books.js
@@ -3,7 +3,8 @@ const BOOKS = [
   {"no":2,"status":"published","title":"64歳、定年後の仕事。01 我人生をどう記録するか","image":"book2.jpg","amazon":"https://www.amazon.co.jp/dp/B0H11RG43L"},
   {"no":3,"status":"published","title":"64歳、定年後の仕事。02 高血圧を薬なしで見ていた記録","image":"book3.jpg","amazon":"https://www.amazon.co.jp/dp/B0H1KS9QG4"},
   {"no":4,"status":"published","title":"64歳、定年後の仕事。03 雑談から生まれたYouTube","image":"book4.jpg","amazon":"https://www.amazon.co.jp/dp/B0GX33FHBL"},
-  {"no":5,"status":"published","title":"どうしたら、1か月でKindle出版できるの？","image":"book5.jpg","amazon":"https://www.amazon.co.jp/dp/B0HJWKN858"}
+  {"no":5,"status":"published","title":"どうしたら、1か月でKindle出版できるの？","image":"book5.jpg","amazon":"https://www.amazon.co.jp/dp/B0HJWKN858"},
+  {"no":6,"status":"published","title":"なぜ私は、儲からない仕事を毎日しているのか？","image":"book6.jpg","amazon":"https://www.amazon.co.jp/dp/B0HJZ3FH7K"}
 ];
 
 // 2ページ目を作成した後、そのWebカタログURLを設定します。
